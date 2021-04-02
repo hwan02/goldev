168c352581faa4229d02d12048ad436b1bf2bd95
seunghwansuh <ssh32009757@gmail.com>
2021-04-02 12:44:48 +0900

    make home

---
+++ website/src/data/successStories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default [
+  {
+    name: 'Preparing...',
+    title: 'Front End Engineer, Facebook',
+    thumbnail: '',
+    quote: (
+      <>
+        Preparing...
+      </>
+    ),
+  },
+];
